5790813f07fb1f35780df54276247e1afa8faff8
Jeroen Engels <jfm.engels@gmail.com>
2020-08-05 19:47:15 +0200

    Don't report errors when there is no custom type

--- tests/NoNoOpMsgTest.elm
+++ tests/NoNoOpMsgTest.elm
@@ -8,7 +8,7 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoNoOpMsg"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report an error when there is a NoOp Msg constructor" <|
             \() ->
                 """module A exposing (..)
 type Msg
@@ -22,4 +22,11 @@ type Msg
                             , under = "NoOp"
                             }
                         ]
+        , test "should not report an error when there is no custom type" <|
+            \() ->
+                """module A exposing (..)
+a = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
         ]
